00b4ae88f9c8b364215205742f1d2b4b103d8b6d
TanzerDx <hristoganchev3@gmail.com>
2025-04-11 01:25:25 +0200

    switched to in-memory db

--- src/test/java/com/pibbletv/follows_service/FollowsServiceApplicationTests.java
+++ src/test/java/com/pibbletv/follows_service/FollowsServiceApplicationTests.java
@@ -3,7 +3,13 @@ package com.pibbletv.follows_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(properties = {
+        "spring.r2dbc.url=r2dbc:h2:mem:///testdb;DB_CLOSE_DELAY=-1",
+        "spring.r2dbc.username=sa",
+        "spring.r2dbc.password=",
+        "spring.r2dbc.pool.enabled=false"
+})
 class FollowsServiceApplicationTests {
 
     @Test
