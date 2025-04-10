fbf026ecf8f58b9bebce32d228d4c8fe3162ce88
TanzerDx <hristoganchev3@gmail.com>
2025-04-11 01:29:15 +0200

    fixed pipeline

--- src/test/java/com/pibbletv/follows_service/FollowsServiceApplicationTests.java
+++ src/test/java/com/pibbletv/follows_service/FollowsServiceApplicationTests.java
@@ -1,15 +1,10 @@
 package com.pibbletv.follows_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
-@SpringBootTest(properties = {
-        "spring.r2dbc.url=r2dbc:h2:mem:///testdb;DB_CLOSE_DELAY=-1",
-        "spring.r2dbc.username=sa",
-        "spring.r2dbc.password=",
-        "spring.r2dbc.pool.enabled=false"
-})
+@ActiveProfiles("test")
 class FollowsServiceApplicationTests {
 
     @Test
