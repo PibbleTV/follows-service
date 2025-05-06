b3ad81791e840bda862672e22931f971f5b289b3
TanzerDx <hristoganchev3@gmail.com>
2025-05-06 23:15:39 +0200

    added following methods

--- src/main/java/com/pibbletv/follows_service/persistance/repository/FollowingRepository.java
+++ src/main/java/com/pibbletv/follows_service/persistance/repository/FollowingRepository.java
@@ -2,6 +2,11 @@ package com.pibbletv.follows_service.persistance.repository;
 
 import com.pibbletv.follows_service.persistance.entities.FollowingEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface FollowingRepository extends ReactiveCrudRepository<FollowingEntity, Long> {
+
+    Flux<FollowingEntity> findByFollowedId(Long followedId);
+
+    Flux<FollowingEntity> findByFollowerId(Long followerId);
 }
